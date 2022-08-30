8f6c0e248422636fd53ebc05b8fcfa839d49c999
alberto-mm <alberto.martin.montero@gmail.com>
2022-08-30 09:51:23 +0200

    Definición del sistema de rutas y navegación

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { Error404Component } from './components/error404/error404.component';
+import { FormComponent } from './components/form/form.component';
+import { UserViewComponent } from './components/user-view/user-view.component';
+import { UsersListComponent } from './components/users-list/users-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'home' },
+  { path: 'home', component: UsersListComponent },
+  { path: 'user/:iduser', component: UserViewComponent },
+  { path: 'newuser', component: FormComponent },
+  { path: 'updateuser/:iduser', component: FormComponent },
+  { path: '**', component: Error404Component }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
